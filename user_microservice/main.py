e6c681ffaf2e28acbe29b7b4a5cb2e6c1ffc0ada
Khalil Farashiani <khalilfarashi2000@gmail.com>
2022-10-27 17:41:45 +0330

    add create tables to create tables database automatically when app start

--- user_microservice/main.py
+++ user_microservice/main.py
@@ -2,9 +2,9 @@ from functools import lru_cache
 
 from fastapi import FastAPI
 
-from routers.apiv1 import user_router
+from routers.apiv1 import user_router, auth_router
 from config import get_settings
-
+from dependency import create_tables
 
 setting = get_settings()
 
@@ -12,6 +12,8 @@ setting = get_settings()
 def create_app():
     user_app = FastAPI(name=setting.APP_NAME, debug=setting.DEBUG)
     user_app.include_router(user_router)
+    user_app.include_router(auth_router)
+    create_tables()
     return user_app
 
 
