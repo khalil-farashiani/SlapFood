18dd444708a5548158577ed94df833565aba6639
khalil_farashiani <khalilfarashi2000@gmail.com>
2023-01-01 22:20:40 +0330

    initial to mongo db as main database product service

---
+++ product_microservice/adapter/store/connection.go
@@ -0,0 +1,35 @@
+package store
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
+)
+
+func InitMongo() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("GET FROM ENV"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(databases)
+
+	return client
+}
--- product_microservice/go.mod
+++ product_microservice/go.mod
@@ -2,4 +2,7 @@ module github.com/khalil-farashiani/SlapFood/product_microservice
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	go.mongodb.org/mongo-driver v1.11.1 // indirect
+)
